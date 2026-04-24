d0c9e875d450ec815dc0401985047a22c235023f
dakshtop <dakshnarang1211@gmail.com>
2026-04-24 18:15:40 +0530

    Update auto-execute.lua

--- auto-execute.lua
+++ auto-execute.lua
@@ -1,1 +1,17 @@
-print("temp")
+queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/dakshtop/Iem/main/auto-execute.lua'))()")
+
+getgenv().Config = {
+    Team = "Pirates",
+    HideUI = true,
+    HuntConfig = {
+        ["Earned Notification Enabled"] = true,
+        ["Reset Farm (New)"] = true,
+        ["Chat"] = false,
+        ["Farm Delay"] = 0.22, -- 0.15 - Super Fast(Risk - Kick) | 0.22 Fast | 0.35 Medium | Max 0.5
+        ["Webhook"] = {
+            Enabled = false,
+            Url = ""
+        }
+    }
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/2ffcdb62773f587bfb9eb0d52bb35b0c.lua"))()
